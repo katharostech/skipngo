ca33b720de8b8b8a1bcdca077b5de02df0d06261
Zicklag <zicklag@katharostech.com>
2021-03-29 18:00:50 -0500

    Hide Terminal on Windows

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+// This hides the terminal window shown when running the app on Windows
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
 fn main() {
     skipngo::run();
 }
